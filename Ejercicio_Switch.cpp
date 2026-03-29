93a507ccfa8e16a2dce4d17ad7ec2b593d36852c
Emmanuel Sanchez <gsancheza7@miumg.edu.gt>
2026-03-29 09:40:06 -0600

    Confirmar

--- Ejercicio_Switch.cpp
+++ Ejercicio_Switch.cpp
@@ -9,7 +9,7 @@ cout << "Este programa ah sido creado por Gezer Emmanuel Sanchez Aj ";
  //Declaramos variables//
  int  Opcion;
 
- //Creamos un ciclo para las opciones//
+ //Creamos el menu de opciones//
  do { 
 cout << "Menu de opciones\n";
 cout << "1. Mostrar numeros del 1 al 5\n";
@@ -23,30 +23,32 @@ cin >> Opcion;
 switch (Opcion) 
 {
 case 1:
-cout <<"Numeros del 1 al 5";
+cout <<"Numeros del 1 al 5: \n";
      for (int i= 1; i <= 5; i++) {
-     cout <<"\n\n";
+          cout << i << " ";
      }
+     cout <<"\n\n";
      break;
 
 case 2:
-cout << "Numeros pares del 2 al 10";
+cout << "Numeros pares del 2 al 10\n";
 for (int i = 2; i <=10; i+=2) {
      cout << i << " ";
 }
+cout << "\n\n";
   break;
 
 case 3:
 int Numero;{
-cout << "Ingrese un numero al azar";
+cout << "Ingrese un numero al azar: ";
 cin >> Numero;
 
 if (Numero > 0) {
-     cout << "El numero es positivo";
+     cout << "El numero es positivo\n\n";
 } else if (Numero < 0) {
-    cout << "El numero es negativo ";
+    cout << "El numero es negativo\n\n";
 } else {
-     cout << "El numero es cero";
+     cout << "El numero es cero\n\n";
 }
 break;
 }
@@ -55,11 +57,12 @@ case 4:
 cout << "Esperemos nuestro programa le haya sido util, bye ";
 break;
 
+//Para el programa vuelva a ejecutar hasta seleccionar la opcion salir//
 default:
-cout << "La opcion ingresada es invalida. Vuelva ingresa una de las opciones visibles ";
+cout << "\nLa opcion ingresada es invalida. Vuelva ingresa una de las opciones visibles\n";
 continue;
 }
- } while (Opcion !=4);
+ } while (Opcion != 4);
 
      
      return 0;
